f15fc6e1bc34cc5520ea60d3c20d3301a947d953
Thomas Gak Deluen <tgdn45@gmail.com>
2022-03-26 12:03:20 +0100

    Fix state indexing issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,10 @@ states = alt.topo_feature(data.us_10m.url, feature="states")
 df = pd.read_csv("./data/state_weapon_assaults.csv", parse_dates=True)
 # Grouping
 # group by state
-assault_count_by_state = df[["state", "count"]].groupby("state").sum()
-assault_count_by_state["id"] = range(1, len(assault_count_by_state) + 1)
+assault_count_by_state = (
+    df[["id", "state", "count"]].groupby(["id", "state"]).sum()
+)
+assault_count_by_state.reset_index(inplace=True)
 
 
 @st.cache
@@ -39,16 +41,25 @@ def build_weapon_pie(state=None):
     total_by_weapon = groupby_weapon(state)
 
     source = pd.DataFrame(
-        {"category": total_by_weapon.index, "value": total_by_weapon["count"]}
+        {"category": total_by_weapon.index, "values": total_by_weapon["count"]}
     )
+    # base = alt.Chart(source).encode(
+    #    theta=alt.Theta("values:Q", stack=True),
+    #    color=alt.Color("category:N", legend=None),
+    # )
+    # pie = base.mark_arc(outerRadius=120)
+    # text = base.mark_text(radius=140, size=10).encode(text="category:N")
+    # return pie + text
     base = alt.Chart(source).encode(
-        theta=alt.Theta("value:Q", stack=True),
+        theta=alt.Theta("values:Q", stack=True),
         color=alt.Color("category:N", legend=None),
+        radius=alt.Radius(
+            "values", scale=alt.Scale(type="sqrt", zero=True, rangeMin=20)
+        ),
     )
-    pie = base.mark_arc(outerRadius=120)
-    text = base.mark_text(radius=140, size=10).encode(text="category:N")
-
-    return pie + text
+    c1 = base.mark_arc(innerRadius=10, stroke="#fff")
+    c2 = base.mark_text(radiusOffset=40).encode(text="category:N")
+    return c1 + c2
 
 
 @st.cache(allow_output_mutation=True)
@@ -72,20 +83,24 @@ st.header("Map of number of killings and assaults in the USA")
 assault_map = build_assault_map()
 st.altair_chart(assault_map, use_container_width=True)
 
-st.header("Assaults outcome per department")
-cars = data.cars()
-chart = (
-    alt.Chart(cars)
-    .mark_bar()
-    .encode(
-        x=alt.X("Miles_per_Gallon:Q", bin=alt.Bin(maxbins=30)),
-        y="count()",
-        color="Origin:N",
+# Two cols
+col1, col2 = st.columns([4, 3])
+with col1:
+    st.subheader("Assaults outcome per department")
+    cars = data.cars()
+    chart = (
+        alt.Chart(cars)
+        .mark_bar()
+        .encode(
+            x=alt.X("Miles_per_Gallon:Q", bin=alt.Bin(maxbins=30)),
+            y="count()",
+            color="Origin:N",
+        )
     )
-)
-st.altair_chart(chart, use_container_width=True)
+    st.altair_chart(chart, use_container_width=True)
 
 
-st.header("Percentage of injuries caused by weapon types")
-pie = build_weapon_pie()
-st.altair_chart(pie, use_container_width=True)
+with col2:
+    st.subheader("Assaults by weapon")
+    pie = build_weapon_pie()
+    st.altair_chart(pie, use_container_width=True)
